e1919cf2eb7d636e6f88904c034e54c49fc7a9e6
ASAN-INJU <jjukssam755@gmail.com>
2026-07-23 04:41:03 +0900

    Import stocks data from stocks.json

--- api-server.js
+++ api-server.js
@@ -7,7 +7,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-
+const stocks = require("./stocks.json");
 const {
     getCurrentPrice,
     getMovingAverage,
